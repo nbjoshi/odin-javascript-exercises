6bf881939fc9bc25d7cf672b19981203f16c2d19
Neel Joshi <nbjoshi@unc.edu>
2024-07-25 17:19:57 -0400

    Stuck on removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    return Array.from(string).reverse().join("");
 
+    // Can also do:
+    // return string.split("").reverse().join("");
+    
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...values) {
+    for (let i = 0; i < values.length; i++) {
+        if (list.includes(values[i])) {
+            let index = list.indexOf(values[i]);
+            list.splice(index, 1);
+        }
+    }
+    return list;
 };
 
 // Do not edit below this line
